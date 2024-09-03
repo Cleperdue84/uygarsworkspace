1beaa74664c5cc53190b6ed041ccdc71b299c4ec
Cleperdue84 <uygaryalcin84@gmail.com>
2024-09-03 13:03:00 +0300

    bunu almanı istiyorum

---
+++ neotech/lesson01/TitleAndLogoValidation.java
@@ -0,0 +1,81 @@
+package com.neotech.lesson01;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.neotech.utils.ConfigsReader;
+
+public class TitleAndLogoValidation extends BaseClass{
+
+	@BeforeMethod
+	public void openAndNavigate()
+	{
+	setUp()	}
+	
+	@AfterMethod
+	public void quitBrowser()
+	{
+	tearDown();
+	}
+	
+	
+	@Test
+	public void titleValidation() throws Exception
+	{
+		String expectedTitle = "OrangeHR"; //intentially failing this test (it should be OrangeHRM)
+		String actualTitle = driver.getTitle();
+		
+		if (expectedTitle.equals(actualTitle))
+		{
+			System.out.println("Title Validation Passed!!!");
+		}
+		else 
+		{
+			System.out.println("Title Validation Failed!!!");
+			System.out.println("The title is: " + actualTitle);
+			throw new Exception(); //later on we will use Assertions
+			//but for now, we can show it as failed by throwing an Exception()
+		}
+		
+	}
+	
+	@Test
+	public void logoValidation()
+	{
+		WebElement logo = driver.findElement(By.xpath("//div[@class='orangehrm-logo']/img"));
+		
+		if (logo.isDisplayed())
+		{
+			System.out.println("Logo Validation Passed!!!");
+		}
+		else 
+		{
+			System.out.println("Logo Validation Failed!!!");
+		}
+	}
+	
+	@Test
+	public void loginValidation()
+	{
+		sendText(driver.findElement(By.id("txtUsername")), ConfigsReader.getProperty("username"));
+		sendText(driver.findElement(By.id("txtPassword")), ConfigsReader.getProperty("password"));
+		
+		click(driver.findElement(By.xpath("//button")));
+		
+		WebElement userImg = driver.findElement(By.xpath("//div[@id='menu-profile']/img"));
+		
+		if(userImg.isDisplayed())
+		{
+			System.out.println("Login Validation Passed!");
+		}
+		else
+		{
+			System.out.println("Login Validation Failed!");
+		}
+		
+	}
+	
+}
